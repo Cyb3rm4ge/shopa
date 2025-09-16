87bf33590f678bf67151f8e564fbeabbe54ca5ce
Cyb3rm4ge <gzneed@gmail.com>
2025-09-16 20:03:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,11 @@ CRYPTOBOT_API_URL = 'https://pay.crypt.bot/api/' # ссылка
 
 DB_CHANNEL_ID = "-1003044061622" # для хранения товаров, обязательно добавить бота в канал
 
+
+TON_WALLET = "UQBDxnZmy0662kr6dKvavqjdDBYgWmL1LjTF1BEF8lCpQYZ-"   # ваш кошелёк
+TON_CHECK_TIMEOUT = 30 * 60   # 30 минут
+TON_API_URL = "https://tonapi.io/v2/blockchain/accounts/{}/transactions"
+
 # ссылки 1 - testnet ; 2 - crypto bot
 # 1 - https://testnet-pay.crypt.bot/api/
 # 2 - https://pay.crypt.bot/api/
