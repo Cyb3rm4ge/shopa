f0c526316224736f5a06e980e03739cef0717326
Cyb3rm4ge <gzneed@gmail.com>
2025-09-16 18:29:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
-TOKEN = "" # токен бота
+TOKEN = "8465105072:AAFntL0v0BIlmxWTPz3b8PgahoT5c3wvodo" # токен бота
 
-ADMIN_ID = 123 # ид админа
+ADMIN_ID = 8465105072:AAFntL0v0BIlmxWTPz3b8PgahoT5c3wvodo # ид админа
 
 CRYPTOBOT_TOKEN = "" # токен crypto pay 
 
 CRYPTOBOT_API_URL = 'https://pay.crypt.bot/api/' # ссылка
 
-DB_CHANNEL_ID = "-100" # для хранения товаров, обязательно добавить бота в канал
+DB_CHANNEL_ID = "-1003044061622" # для хранения товаров, обязательно добавить бота в канал
 
 # ссылки 1 - testnet ; 2 - crypto bot
 # 1 - https://testnet-pay.crypt.bot/api/
